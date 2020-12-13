4829bdefdd066f86ef5b759aecfdd50ae75a7df5
suelion <lsb622@naver.com>
2020-12-13 22:57:32 +0900

    sql 첫 연습

---
+++ 20200402_SQL_test_연습1.sql
@@ -0,0 +1,17 @@
+INSERT INTO test_db VALUES (1, '이수빈','데이터베이스 수업 받는 중') 
+
+SELECT * FROM test_db
+
+SELECT VALUE FROM test_db
+
+SELECT VALUE, comment FROM test_db
+
+SELECT num, VALUE FROM test_db
+
+INSERT INTO test_db (num, VALUE) VALUES (2, '영희')
+
+INSERT INTO test_db (num) VALUES (3)
+
+SELECT num, VALUE FROM test_db
+
+SELECT VALUE, comment FROM test_dbbigdatabigdatamoviemoviebigdata
