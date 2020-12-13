dad96fbe89f9de19149e4d0942f169eaf84ce1f1
suelion <lsb622@naver.com>
2020-12-13 22:13:53 +0900

    간단한 계산
    +,-,*,/

---
+++ Code02-02.py
@@ -0,0 +1,53 @@
+a = int(input("첫 번째 숫자를 입력하세요 : "))
+b = int(input("두 번째 숫자를 입력하세요 : "))
+
+'''
+a = input()
+b = input()
+a = int(a)
+b = int(b)
+'''
+
+result = a + b
+print(a, "+", b, "=", result)
+result = a - b
+print(a, "-", b, "=", result)
+result = a * b
+print(a, "*", b, "=", result)
+result = a / b
+print(a, "/", b, "=", result)
+
+
+
+
+
+
+"""import turtle
+
+turtle.shape('turtle')
+
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+
+turtle.done()"""
+
+
+
+"""import turtle
+
+myT=None
+
+myT = turtle.Turtle()
+myT.shape('turtle')
+
+for i in range(0, 4) :
+    myT.forward(200)
+    myT.right(90)
+
+turtle.done()"""
